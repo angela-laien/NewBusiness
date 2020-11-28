d6ad2b3f34a7517a5dcdd2e18357393e57f0c3fe
angela-laien <laienxie@gmail.com>
2020-11-27 19:27:01 -0800

    added sendgrid

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+import sendgrid
+from sendgrid.helpers.mail import Email
+from sendgrid.helpers.mail import Content
+from sendgrid.helpers.mail import Mail
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index.html")
+@app.route("/", methods=['POST', 'GET'])
+@app.route("/index.html", methods=['POST', 'GET'])
 
 def index():
+    if request.method == 'POST':
+        email_address = request.form['email']
+        full_name = request.form['name']
+        message = request.form['message']
+
+        if email_address != '':
+            full_name = full_name.encode('ascii', errors="ignore").decode()
+
+            SENDGRID_API_KEY = os.getenv('API_KEYS')
+            sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_API_KEY)
+
+            message = Mail(
+                from_email='laienxie@gmail.com',
+                to_emails='laienxie@gmail.com',
+                subject='New SFPreloved.com Message',
+                html_content='Email: ' + email_address + ' Name: ' + full_name + ' ' + message
+            )
+
+            try:
+                sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_API_KEY)
+                response = sg.send(message)
+            except Exception as e:
+                print(str(e))
+
     return render_template("index.html")
